b7f024abd0e8743044c5071c364741b0b9f75991
HIMANSHU-GAUTAM123 <98645159+HIMANSHU-GAUTAM123@users.noreply.github.com>
2022-10-16 11:50:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resume from './components/resume';
+import Studentanalys from './components/studentanalys';
+const App = () => {
+  return (
+    <div>
+    <Studentanalys/>
+    </div>
+  )
+}
+
+export default App
